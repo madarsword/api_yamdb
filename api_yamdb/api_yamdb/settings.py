f012375547088c94f3ac6c2aa2e9a3a3a03c749a
al-shp <alshp@tutanota.com>
2023-04-28 19:28:22 +0200

    fix app

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -17,7 +17,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'api.apps.AppConfig',
+    'api.apps.ApiConfig',
     'reviews.apps.ReviewsConfig',
 ]
 
