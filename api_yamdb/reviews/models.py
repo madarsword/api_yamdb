bf10558540b8394bba576a2dc30089de685593a9
Fazil Israfilov <fisrafilov@Fazils-MacBook-Air.local>
2023-04-29 19:49:57 +0300

    Добавил класс Comment и внес несколько дополнений в Review

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -5,18 +5,17 @@ from django.core.validators import (
 )
 from django.contrib.auth import get_user_model
 
-
-User = get_user_model()  # удалить и заменить на
-                         # Саниного абстракт юзера
+User = get_user_model()
 
 
 class Review(models.Model):
 
     title = models.ForeignKey(
-        Title,  # нужен мердж по Теминому пул реквесту
+        Title,
         on_delete=models.CASCADE,
         related_name='reviews',
         verbose_name='Произведение',
+        null=True,
     )
     text = models.TextField(
         verbose_name='Текст отзыва',
@@ -27,14 +26,14 @@ class Review(models.Model):
         related_name='reviews',
         verbose_name='Автор отзыва',
     )
-    score = models.IntegerField(
+    score = models.PositiveSmallIntegerField(
         verbose_name='Оценка',
         validators=[
-            MinValueValidator(  # выглядит как костыль. Подумайте тоже плиз
+            MinValueValidator(
                 1,
                 message='Оценка должна быть больше 1',
             ),
-            MaxValueValidator(  # аналогично
+            MaxValueValidator(
                 10,
                 message='Оценка должна быть меньше 10',
             ),
@@ -44,3 +43,40 @@ class Review(models.Model):
         auto_now_add=True,
         verbose_name='Дата публикации отзыва',
     )
+
+    class Meta:
+        verbose_name = 'Отзыв',
+        verbose_name_plural = 'Отзывы',
+
+    def __str__(self):
+        return self.text[:15]
+
+
+class Comment(models.Model):
+
+    text = models.TextField(
+        verbose_name='Текст комментария',
+    )
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='comments',
+        verbose_name='Автор комментария',
+    )
+    pub_date = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name='Дата публикации комментария',
+    )
+    review = models.ForeignKey(
+        Review,
+        on_delete=models.CASCADE,
+        related_name='comments',
+        verbose_name='Отзыв',
+    )
+
+    class Meta:
+        verbose_name = 'Комментарий',
+        verbose_name_plural = 'Комментарии',
+    
+    def __str__(self):
+        return self.text[:15]
