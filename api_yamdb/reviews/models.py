269af733e529231168a2a467a97c706fa6a5d038
al-shp <alshp@tutanota.com>
2023-04-30 14:24:57 +0200

    vse

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -2,7 +2,6 @@ from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
 from django.contrib.auth import get_user_model
 
-from .models import Title
 from .validators import validate_year
 
 
