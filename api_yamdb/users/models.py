ebf95f087dd7aa2d2d0fb665c726fb94d96b0d97
al-shp <alshp@tutanota.com>
2023-05-02 14:51:10 +0200

    .

--- api_yamdb/users/models.py
+++ api_yamdb/users/models.py
@@ -40,7 +40,7 @@ class User(AbstractUser):
     
     @property
     def is_admin(self):
-        return self.role == "admin"
+        return self.role == "admin" or self.is_superuser
 
     @property
     def is_moderator(self):
