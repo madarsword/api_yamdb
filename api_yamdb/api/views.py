91ce1fca757b1d9987f19a10834b3c9eb2317262
al-shp <alshp@tutanota.com>
2023-05-02 10:24:48 +0200

    fix

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,10 +1,7 @@
 from django.shortcuts import get_object_or_404
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import filters, mixins, status, viewsets
-from rest_framework.response import Response
-from rest_framework.views import APIView
-from rest_framework.permissions import (IsAuthenticated,
-                                        IsAuthenticatedOrReadOnly)
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 from reviews.models import Category, Genre, Review, Title
 from users.models import User
