065265608f553ff816719585fc1447f4fbc36ffa
al-shp <alshp@tutanota.com>
2023-05-02 11:09:31 +0200

    +11

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import get_object_or_404
+from django.db.models import Avg
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import filters, mixins, viewsets
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
--- api_yamdb/users/validators.py
+++ api_yamdb/users/validators.py
@@ -2,5 +2,5 @@ from django.core.validators import RegexValidator
 
 
 class UsernameValidator(RegexValidator):
-    regex = r'^[\w.@+-]+\z'
+    regex = r'^[\w.@+-]'
     flags = 0
