dc1204f1e4c9c2472b19642ba93121994a252a90
al-shp <alshp@tutanota.com>
2023-05-03 23:48:15 +0200

    final

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -12,7 +12,7 @@ class IsAdmin(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         user = request.user
         return (
-            user.is_authenticated 
+            user.is_authenticated
             and (user.is_admin or user.is_superuser)
         )
 
